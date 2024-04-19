ce1db8d939efd3b39145b433a9f6b95748cb7678
Niklas L <niklas@demtrees.com>
2024-04-19 14:28:22 +0300

    Hook to Replicate and Stability AI to generate variants from main image

--- supabase/functions/generate/index.ts
+++ supabase/functions/generate/index.ts
@@ -6,11 +6,18 @@
 import { corsHeaders } from "../_shared/cors.ts";
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2.42.3";
 import randomSample from "https://esm.sh/@stdlib/random-sample@0.2.1";
+import Replicate from "https://esm.sh/replicate@0.29.1";
+import { base64 } from "https://cdn.jsdelivr.net/gh/hexagon/base64@1/src/base64.js";
 
 const supabaseClient = createClient(
   Deno.env.get("SUPABASE_URL") ?? "",
   Deno.env.get("SUPABASE_SERVICE_ROLE_KEY") ?? ""
 );
+
+const replicate = new Replicate({
+  auth: Deno.env.get("REPLICATE_API_TOKEN") ?? "",
+});
+
 // @ts-expect-error
 Deno.serve(async (req) => {
   if (req.method === "OPTIONS") {
@@ -29,8 +36,8 @@ Deno.serve(async (req) => {
   );
 
   const { objectIDs } = await allObjectIDsResponse.json();
-  let listOfArtworks = [];
-  let addedIDs: number[] = [];
+  const listOfArtworks = [];
+  const addedIDs: number[] = [];
 
   while (listOfArtworks.length < 20) {
     const randomObjectID = randomSample(objectIDs, { size: 1 })[0];
@@ -48,7 +55,7 @@ Deno.serve(async (req) => {
       }
     );
 
-    let artwork = await res.json();
+    const artwork = await res.json();
 
     if (!artwork.primaryImageSmall) continue;
 
@@ -56,15 +63,81 @@ Deno.serve(async (req) => {
 
     listOfArtworks.push({
       image_url: artwork.primaryImageSmall,
+      artist_name: artwork.artistDisplayName,
       is_main: listOfArtworks.length === 0,
       is_variant: false,
+      // TODO: Change "1" to be the actual thought ID
       thought_id: 1,
     });
   }
 
+  const mainImage = listOfArtworks[0];
+
+  const output = await replicate.run(
+    "yorickvp/llava-13b:b5f6212d032508382d61ff00469ddda3e32fd8a0e75dc39d8a4191bb742157fb",
+    {
+      input: {
+        image: mainImage.image_url,
+        top_p: 1,
+        prompt: "Describe this painting by " + mainImage.artist_name,
+        max_tokens: 1024,
+        temperature: 0.2,
+      },
+    }
+  );
+
+  const file = await fetch(mainImage.image_url).then((res) => res.blob());
+  const promises = [];
+
+  for (let i = 0; i < 7; i++) {
+    const body = new FormData();
+    body.append(
+      "prompt",
+      output.join("") + `, a painting by ${mainImage.artist_name}`
+    );
+    body.append("output_format", "jpeg");
+    body.append("mode", "image-to-image");
+    body.append("image", file);
+    body.append("strength", clamp(Math.random(), 0.4, 0.7));
+
+    const request = fetch(
+      `${Deno.env.get(
+        "STABLE_DIFFUSION_HOST"
+      )}/v2beta/stable-image/generate/sd3`,
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          Authorization: `Bearer ${Deno.env.get("STABLE_DIFFUSION_API_KEY")}`,
+        },
+        body,
+      }
+    );
+
+    promises.push(request);
+  }
+
+  const results = await Promise.all(promises);
+  const variants = await Promise.all(results.map((res) => res.json()));
+
   await supabaseClient.from("artworks").insert(listOfArtworks);
 
-  return new Response(JSON.stringify({ randomIDs: listOfArtworks }), {
+  for (let i = 0; i < variants.length; i++) {
+    const variant = variants[i];
+
+    await supabaseClient.storage
+      .from("variants")
+      // TODO: Change "1" to be the actual thought ID
+      .upload(`1/${Math.random()}.jpeg`, base64.toArrayBuffer(variant.image), {
+        contentType: "image/jpeg",
+      });
+  }
+
+  return new Response(JSON.stringify({ main: mainImage }), {
     headers: { "Content-Type": "application/json", ...corsHeaders },
   });
 });
+
+function clamp(value: number, min: number, max: number) {
+  return Math.min(Math.max(value, min), max);
+}
