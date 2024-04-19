02a3c9c83819b82becc96a1e19a0d6cfae82b260
Niklas L <niklas@demtrees.com>
2024-04-19 13:04:19 +0300

    Add initial functionality to store artworks to Supabase

--- supabase/functions/generate/index.ts
+++ supabase/functions/generate/index.ts
@@ -5,6 +5,7 @@
 
 import { corsHeaders } from "../_shared/cors.ts";
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2.42.3";
+import randomSample from "https://esm.sh/@stdlib/random-sample@0.2.1";
 
 const supabaseClient = createClient(
   Deno.env.get("SUPABASE_URL") ?? "",
@@ -17,7 +18,7 @@ Deno.serve(async (req) => {
   }
 
   const allObjectIDsResponse = await fetch(
-    "https://collectionapi.metmuseum.org/public/collection/v1/objects",
+    "https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=11",
     {
       method: "GET",
       headers: {
@@ -28,8 +29,42 @@ Deno.serve(async (req) => {
   );
 
   const { objectIDs } = await allObjectIDsResponse.json();
+  let listOfArtworks = [];
+  let addedIDs: number[] = [];
 
-  return new Response(JSON.stringify({ hello: objectIDs }), {
+  while (listOfArtworks.length < 20) {
+    const randomObjectID = randomSample(objectIDs, { size: 1 })[0];
+
+    if (addedIDs.includes(randomObjectID)) continue;
+
+    const res = await fetch(
+      `https://collectionapi.metmuseum.org/public/collection/v1/objects/${randomObjectID}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      }
+    );
+
+    let artwork = await res.json();
+
+    if (!artwork.primaryImageSmall) continue;
+
+    addedIDs.push(artwork.objectID);
+
+    listOfArtworks.push({
+      image_url: artwork.primaryImageSmall,
+      is_main: listOfArtworks.length === 0,
+      is_variant: false,
+      thought_id: 1,
+    });
+  }
+
+  await supabaseClient.from("artworks").insert(listOfArtworks);
+
+  return new Response(JSON.stringify({ randomIDs: listOfArtworks }), {
     headers: { "Content-Type": "application/json", ...corsHeaders },
   });
 });
