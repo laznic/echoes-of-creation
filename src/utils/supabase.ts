d466a50e72e4b09abc1bce1810a0de894f014aa4
Niklas L <niklas@demtrees.com>
2024-04-20 00:43:03 +0300

    Add new effects and adjust things

---
+++ src/utils/supabase.ts
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  import.meta.env.PUBLIC_SUPABASE_URL,
+  import.meta.env.PUBLIC_SUPABASE_ANON_KEY
+);
+
+export default supabase;
